9eedaf933d0d28f591c510943dc904d9bcca1e44
lobodae <emilkaloboda@wp.pl>
2023-06-10 13:33:21 +0200

    nowa baza

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 31 Maj 2023, 18:14
+-- Czas generowania: 10 Cze 2023, 13:31
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -31,19 +31,26 @@ CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `category` varchar(20) NOT NULL
+  `category` varchar(20) NOT NULL,
+  `nr_polki` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `nr_regal` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `alphabet` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `author`, `category`) VALUES
-(10, 'Hobbit', 'Tolkien', 'fantasy'),
-(11, 'IT', 'Ktos', 'horror'),
-(12, 'Szybcy i wsciekli', 'wiadomo', 'sci-fi'),
-(13, 'moje', 'życie', 'comedy'),
-(14, 'szkoła', '1', 'documentary');
+INSERT INTO `books` (`id`, `title`, `author`, `category`, `nr_polki`, `nr_regal`, `alphabet`) VALUES
+(10, 'Hobbit', 'Tolkien', 'fantasy', '', '', ''),
+(11, 'IT', 'Ktos', 'horror', '', '', ''),
+(12, 'Szybcy i wsciekli', 'wiadomo', 'sci-fi', '', '', ''),
+(13, 'moje', 'życie', 'comedy', '', '', ''),
+(14, 'szkoła', '1', 'documentary', '', '', ''),
+(15, 'test', 'test', '1', '', '', ''),
+(16, 'test00', 'test00', 'horror', '3', 'R2', 'C'),
+(17, 'test4', 'test4', 'horror', '3', '', ''),
+(18, 'testowy5', 'uwu', 'sci-fi', '1', '1', 'D');
 
 -- --------------------------------------------------------
 
@@ -54,6 +61,7 @@ INSERT INTO `books` (`id`, `title`, `author`, `category`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `email` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -61,10 +69,27 @@ CREATE TABLE `users` (
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin123'),
-(2, 'filip', 'kox'),
-(3, '1234', '1234');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'admin', '', 'admin123'),
+(2, 'filip', '', 'kox'),
+(3, '1234', '', '1234'),
+(4, 'user1', 'user1@wp.pl', 'user1'),
+(5, 'user2', 'user2@wp.pl', 'user2'),
+(6, 'user3', 'user3@wp.pl', 'user3'),
+(7, 'user4', 'user3@wp.pl', 'user3'),
+(8, 'userrrr', 'user44@wp.pl', 'user44'),
+(9, 'userrrr4', 'user44@wp.pl', 'user44'),
+(10, 'test1', 'test@wp.pl', 'test'),
+(11, 'test12', 'test@wp.pl', 'test'),
+(12, 'user000', 'user0@wp.pl', 'user333'),
+(13, 'user1111', 'user111111@wp.pl', 'user00000'),
+(14, 'ooooooooooooo', 'ooooo@wo.pl', '00000'),
+(15, 'u8', 'u8@wp.pl', 'iiiiiiiiii'),
+(16, 'user13', 'user13@wp.pl', 'user13'),
+(17, 'user222222', 'user1@wp.pl', 'user2'),
+(18, 'userrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', 'user15@po.pl', 'User13#'),
+(19, 'userR', 'user09@wp.pl', 'User1444$'),
+(20, 'testowy1', 'testowy11@wp.pl', '$argon2id$v=19$m=65536,t=4,p=1$VGhqMjU5Mmp3eEo1UGxsaQ$tbJMnaGR8RQ4iwRGUN2GyGMS7j6VvQEPt0sbBOaHM7Y');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -90,13 +115,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
