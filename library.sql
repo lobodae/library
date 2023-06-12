40a9b9896ab45b87cda5f5c78ee21dae21c04ea1
lobodae <emilkaloboda@wp.pl>
2023-06-12 22:15:33 +0200

    nowa baza

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Cze 2023, 13:31
+-- Czas generowania: 12 Cze 2023, 22:14
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -43,14 +43,28 @@ CREATE TABLE `books` (
 
 INSERT INTO `books` (`id`, `title`, `author`, `category`, `nr_polki`, `nr_regal`, `alphabet`) VALUES
 (10, 'Hobbit', 'Tolkien', 'fantasy', '', '', ''),
-(11, 'IT', 'Ktos', 'horror', '', '', ''),
 (12, 'Szybcy i wsciekli', 'wiadomo', 'sci-fi', '', '', ''),
 (13, 'moje', 'życie', 'comedy', '', '', ''),
-(14, 'szkoła', '1', 'documentary', '', '', ''),
-(15, 'test', 'test', '1', '', '', ''),
-(16, 'test00', 'test00', 'horror', '3', 'R2', 'C'),
-(17, 'test4', 'test4', 'horror', '3', '', ''),
-(18, 'testowy5', 'uwu', 'sci-fi', '1', '1', 'D');
+(14, '2222', 'ssss', 'horror', '2', 'R2', 'D');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` tinyint(4) NOT NULL,
+  `role` enum('user','moderator','administrator') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`) VALUES
+(1, 'administrator'),
+(2, 'user');
 
 -- --------------------------------------------------------
 
@@ -62,34 +76,32 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `role_id` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'admin', '', 'admin123'),
-(2, 'filip', '', 'kox'),
-(3, '1234', '', '1234'),
-(4, 'user1', 'user1@wp.pl', 'user1'),
-(5, 'user2', 'user2@wp.pl', 'user2'),
-(6, 'user3', 'user3@wp.pl', 'user3'),
-(7, 'user4', 'user3@wp.pl', 'user3'),
-(8, 'userrrr', 'user44@wp.pl', 'user44'),
-(9, 'userrrr4', 'user44@wp.pl', 'user44'),
-(10, 'test1', 'test@wp.pl', 'test'),
-(11, 'test12', 'test@wp.pl', 'test'),
-(12, 'user000', 'user0@wp.pl', 'user333'),
-(13, 'user1111', 'user111111@wp.pl', 'user00000'),
-(14, 'ooooooooooooo', 'ooooo@wo.pl', '00000'),
-(15, 'u8', 'u8@wp.pl', 'iiiiiiiiii'),
-(16, 'user13', 'user13@wp.pl', 'user13'),
-(17, 'user222222', 'user1@wp.pl', 'user2'),
-(18, 'userrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', 'user15@po.pl', 'User13#'),
-(19, 'userR', 'user09@wp.pl', 'User1444$'),
-(20, 'testowy1', 'testowy11@wp.pl', '$argon2id$v=19$m=65536,t=4,p=1$VGhqMjU5Mmp3eEo1UGxsaQ$tbJMnaGR8RQ4iwRGUN2GyGMS7j6VvQEPt0sbBOaHM7Y');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role_id`) VALUES
+(1, 'admin', '', 'admin123', 1),
+(2, 'filip', '', 'kox', 1),
+(3, '1234', '', '1234', 1),
+(4, 'user1', 'user1@wp.pl', 'user1', 2),
+(5, 'user2', 'user2@wp.pl', 'user2', 1),
+(6, 'user3', 'user3@wp.pl', 'user3', 1),
+(7, 'user4', 'user3@wp.pl', 'user3', 1),
+(8, 'userrrr', 'user44@wp.pl', 'user44', 1),
+(9, 'userrrr4', 'user44@wp.pl', 'user44', 1),
+(10, 'test1', 'test@wp.pl', 'test', 1),
+(11, 'test12', 'test@wp.pl', 'test', 1),
+(12, 'user000', 'user0@wp.pl', 'user333', 1),
+(13, 'user1111', 'user111111@wp.pl', 'user00000', 1),
+(15, 'u8', 'u8@wp.pl', 'iiiiiiiiii', 1),
+(16, 'user13', 'user13@wp.pl', 'user13', 1),
+(17, 'user222222', 'user1@wp.pl', 'user2', 1),
+(21, 'userekserek', 'userek1@wp.pl', '$argon2id$v=19$m=65536,t=4,p=1$eXJKQ1ZNQUg0T25PbXdSeA$mLAHuEkv4gqNtXEL7JU5mLucNvDclUhcyZbTQxaEaVI', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -102,10 +114,17 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -118,10 +137,16 @@ ALTER TABLE `books`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
+-- AUTO_INCREMENT dla tabeli `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
